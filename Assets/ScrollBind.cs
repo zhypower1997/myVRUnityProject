1fbc8719e716393024ac41ed10e2d5fc741572ec
zhypower1997 <854091383@qq.com>
2021-04-13 10:35:12 +0800

    first commit

---
+++ Assets/ScrollBind.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScrollBind: MonoBehaviour {
+    public ScrollRect scrollView;
+    public void onMyFun(PointerEventData eventData)
+    {
+        scrollView.OnDrag(eventData);
+        Debug.Log(eventData);
+    }
+
+    public void MyFun(PointerEventData a)
+    {
+        Debug.Log(1);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+
+    }
+
+
+}
